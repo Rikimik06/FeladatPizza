42c0d4fe7183496f49e5f52a1c4ead9be708f56e
Rikimik06 <tothpatrikpeter@gmail.com>
2025-11-25 13:16:24 +0100

    Finishing the project

--- PizzaFeladat/MainWindow.xaml.cs
+++ PizzaFeladat/MainWindow.xaml.cs
@@ -1,24 +1,45 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace PizzaFeladat
+namespace PizzaRendeles
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void RendelesClick(object sender, RoutedEventArgs e)
+		{
+			string teszta = (lbTesztak.SelectedItem as ListBoxItem)?.Content.ToString()
+							?? "Nincs kiválasztva";
+
+			string meret = (cbMeret.SelectedItem as ComboBoxItem).Content.ToString();
+
+			List<string> feltetek = new List<string>();
+			if (chSajt.IsChecked == true) feltetek.Add("Sajt");
+			if (chSonka.IsChecked == true) feltetek.Add("Sonka");
+			if (chGomba.IsChecked == true) feltetek.Add("Gomba");
+			if (chOlivabogyo.IsChecked == true) feltetek.Add("Olívabogyó");
+			if (chKukorica.IsChecked == true) feltetek.Add("Kukorica");
+
+			string feltetSzoveg = feltetek.Count > 0 ? string.Join(", ", feltetek)
+													 : "Nincs plusz feltét";
+
+			string atvetel = rbHazhoz.IsChecked == true
+				? "Házhozszállítás"
+				: "Személyes átvétel";
+
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"Tészta: {teszta}");
+			sb.AppendLine($"Méret: {meret}");
+			sb.AppendLine($"Feltétek: {feltetSzoveg}");
+			sb.AppendLine($"Átvételi mód: {atvetel}");
+
+			txtOsszegzes.Text = sb.ToString();
+		}
+	}
 }
